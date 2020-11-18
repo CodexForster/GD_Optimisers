803b0e288ade1a3a452c23e4ee380050fed4beee
Danush Shekar <67160286+CodexForster@users.noreply.github.com>
2020-11-18 09:26:08 +0530

    Add files via upload

--- comparision.py
+++ comparision.py
@@ -10,18 +10,19 @@ x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 y1 = np.array([45.66, 55.59, 58.32, 59.22, 59.73, 60.0, 60.35, 60.31, 60.23, 60.35])
 
 #Plain SGD
-y2 = np.array([70.59, 71.41, 71.52, 71.45, 71.48, 71.41, 71.48, 71.25])
+y2 = np.array([70.59, 71.41, 71.52, 71.45, 71.48, 71.41, 71.48, 71.25]) # result crashed after 8 epochs
+#y2 = np.array([66.13, 68.09, 68.75, 69.18, 69.84, 69.49, 69.22]) # results crashed after 7 epochs
 
 #MBGD
 y3 = np.array([46.25, 55.82, 58.87, 59.65, 60.39, 60.47, 60.78, 61.06, 61.21, 61.41])
 
 #Adam mini-batch (both -ve updates)
-#y4 = np.array([46.95, 52.31, 60.08, 65.20, 65.32]) old result for 5 epochs
+#y4 = np.array([46.95, 52.31, 60.08, 65.20, 65.32]) # old result for 5 epochs
 y4 = np.array([46.52, 52.07, 60.55, 65.08, 65.31, 65.55, 65.82, 66.13, 66.48, 66.45])
 #y4 = np.array([25.43, 22.42, 20.82, 20, 19.96])  #new dLdW update
 
 #Adam SGD
-y5 = np.array([67.77, 69.26, 70.08, 70.74, 71.09])
+y5 = np.array([67.77, 69.26, 70.08, 70.74, 71.09, 70.20, 70.78, 70.74]) # result crashed after 8 epochs
 
 #Adam mini-batch (one +ve one -ve update)
 y6 = np.array([26.52, 27.11, 29.53, 31.72, 34.65])
@@ -35,8 +36,7 @@ ax1.plot(x, y1, label='HerBo with minibatch size 128')
 ax1.plot(range(1,9), y2, label='Plain SGD')
 ax1.plot(x, y3, label='MBGD')
 ax1.plot(x, y4, label='Adam with minibatch size 128')
-#ax1.plot(range(1,6), y5, label='Adam SGD')
-#ax1.plot(x, y6, label='Adam with minibatch size 128')
+ax1.plot(range(1,9), y5, label='Adam SGD')
 plt.legend(loc='best')
 plt.title('Comparision ((Training size, testing size) = (2560, 2560))')
 plt.show()
